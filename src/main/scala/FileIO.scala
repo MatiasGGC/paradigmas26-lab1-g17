773732d35c649bbd1ae8431921b3839d0bd2ea15
diegosc41 <diegojscacchi@gmail.com>
2026-03-30 23:51:22 -0300

    progress: adding exercise 1

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -1,14 +1,24 @@
 import scala.io.Source
+import scala.util.Using
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
 object FileIO {
-  // Pure function to read subscriptions from a JSON file
-  def readSubscriptions(): List[String] = {
-    List(
-      "https://www.reddit.com/r/scala/.json?count=10",
-      "https://www.reddit.com/r/learnprogramming/.json?count=10"
-    )
-  }
+  type Subscription = (String, String)
 
+  // construimos una case class conveniente para parsear 
+  private case class SubscriptionJson(name: String, url: String)
+
+  // formateo estandar para json4s  
+  implicit val formats: Formats = DefaultFormats
+
+  def readSubscriptions(path: String): List[Subscription] =
+    Using.resource(Source.fromFile(path)) { source =>
+      parse(source.mkString)
+        .extract[List[SubscriptionJson]]
+        .map(s => (s.name, s.url))
+    }
+  
   // Pure function to download JSON feed from a URL
   def downloadFeed(url: String): String = {
     val source = Source.fromURL(url)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,9 +2,10 @@ object Main {
   def main(args: Array[String]): Unit = {
     val header = s"Reddit Post Parser\n${"=" * 40}"
 
-    val subscriptions: List[String] = FileIO.readSubscriptions()
+    // no agregamos la salida :List(Subscription) ya que fue definida en FileIO
+    val subscriptions = FileIO.readSubscriptions("subscriptions.json")
 
-    val allPosts: List[(String, String)] = subscriptions.map { url =>
+    val allPosts: List[(String, String)] = subscriptions.map { case (name, url) =>
       println(s"Fetching posts from: $url")
       val posts = FileIO.downloadFeed(url)
       (url, posts)
