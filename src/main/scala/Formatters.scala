60723633b0b7201bbfe821c0c2bba31b5a57120a
diegosc41 <diegojscacchi@gmail.com>
2026-04-05 18:09:19 -0300

    quick fix

--- src/main/scala/Formatters.scala
+++ src/main/scala/Formatters.scala
@@ -1,10 +1,20 @@
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
 object Formatters {
+  type Post = (String, String, String, String) // subreddit, title, selftext, created
+
+  implicit val formats: Formats = DefaultFormats
 
   // Pure function to format posts from a subscription
-  def formatSubscription(url: String, posts: String): String = {
-    val header = s"\n${"=" * 80}\nPosts from: $url \n${"=" * 80}"
-    val formattedPosts = posts.take(80)
-    header + "\n" + formattedPosts
-  }
+  def formatSubscription(url: String, posts: String): List[Post] =
+    (parse(posts) \ "data" \ "children")
+    .extract[List[JValue]]
+    .map { post => 
+    ((post \ "data" \ "subreddit").extract[String],
+    (post \ "data" \ "title").extract[String],
+    (post \ "data" \ "selftext").extract[String],
+    (post \ "data" \ "created_utc").extract[String])
+    //(TextProcessing.formatDateFromUTC((post \ "data" \ "created_utc").extract[Double].toLong)))
+    }
 }
