e8fb40b92fd7f234f7a0b60a95cd55d6fe246893
emiandres <emilia.andres@mi.unc.edu.ar>
2026-04-06 17:40:11 -0300

    feat: implement PostFilter to clean invalid posts
    Filtered posts that:
    -Have empty selftext
    -Contain oly whitespace
    -Have no title

---
+++ src/main/scala/PostFilter.scala
@@ -0,0 +1,20 @@
+import Formatters.Post
+
+object PostFilter{ //usamos esto para agrupar funciones relacionadas sin necesidad de crear instancias //subreddit, title, selftext, created
+
+    val esEspacio: Char => Boolean =
+    c => c == ' ' || c == '\n' || c == '\t'
+
+    val quitarEspacios: String => String =
+        texto => texto.filter(c => !esEspacio(c))
+
+    val tieneContenidoReal: Option[String] => Boolean =
+        textoOpt => textoOpt.exists(texto => quitarEspacios(texto).nonEmpty)
+
+    val esValido: Post => Boolean = 
+        post => tieneContenidoReal(post._2) && tieneContenidoReal(post._3)
+
+    val filtrarPost: List[Post] => List[Post] =
+        posts => posts.filter(esValido)
+
+}
